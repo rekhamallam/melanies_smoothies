025d3d3bbb13b7f112f89ba146eea4857381f494
rekhamallam <48397892+rekhamallam@users.noreply.github.com>
2025-10-29 15:49:42 -0400

    Fix API request URL in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     for FRUIT_NAME in ingredients_list:
         ingredients_list += FRUIT_NAME + ' '
         st.subheader(FRUIT_NAME + 'Nutrition Information')
-        smoothiefroot_response = requests.get(""https://my.smoothiefroot.com/api/fruit/" + FRUIT_NAME)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + FRUIT_NAME)
         sf_df =st.dataframe(data=smothieifroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS,name_on_order)
     values ('""" + ingredients_list + """','""" + name_on_order + """');"""
