ef3b08e5ec2d044240a5256874db132d39808356
rekhamallam <48397892+rekhamallam@users.noreply.github.com>
2025-10-29 19:43:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-    st.dataframe(pd_df)
+st.dataframe(pd_df)
     st.stop()
 # Extract the value from each Row object into a simple Python list
 ingredients_list = st.multiselect(
