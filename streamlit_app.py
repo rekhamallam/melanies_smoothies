8edb2a91b91e9afba9932d50820b8e35960bdebd
rekhamallam <48397892+rekhamallam@users.noreply.github.com>
2025-10-29 19:16:31 -0400

    Display fruit options in Streamlit app
    Fix dataframe conversion and display in Streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-#convert the snowpark dataframe to panda dataframw
-pd_df=my_dataframe.to_pandas()
+
 st.dataframe(pd_df)
 st.stop()
 # Extract the value from each Row object into a simple Python list
