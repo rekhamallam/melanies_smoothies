a7c53f73e4d690bd02a925150e7ef2cb28fa176e
rekhamallam <48397892+rekhamallam@users.noreply.github.com>
2025-10-29 19:10:31 -0400

    Update default name on order and refactor fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,30 +5,23 @@ from snowflake.snowpark.functions import col
 import requests
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-
 # Write directly to the app
 st.title(f"🥤 Customize Your Smoothie 🥤 {st.__version__}")
 st.write(
     """ Choose any 5 fruits!
     """
 )
-
-
-name_on_order = st.text_input("Name on Order", "Mellymel")
+name_on_order = st.text_input("Name on Order", "adya")
 st.write("The currentName of Order is", name_on_order)
-
 cnx = st.connection("snowflake")
 #session = get_active_session()
 session = cnx.session()
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-
 #convert the snowpark dataframe to panda dataframw
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
-
 # Extract the value from each Row object into a simple Python list
 ingredients_list = st.multiselect('Choose any 5 fruits!',my_dataframe, max_selections =5) 
 st.write(ingredients_list)
@@ -37,14 +30,10 @@ if ingredients_list:
     
     
     ingredients_string = ''
-    for FRUIT_NAME in ingredients_list:
-        ingredients_string += FRUIT_NAME + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == FRUIT_NAME, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', FRUIT_NAME,' is ', search_on, '.')
-
-        st.subheader(FRUIT_NAME + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + FRUIT_NAME)
+    for FRUIT_CHOSEN in ingredients_list:
+        ingredients_string += FRUIT_CHOSEN + ' '
+        st.subheader(FRUIT_CHOSEN + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + FRUIT_CHOSEN)
         #sf_df = st.dataframe(data=smothieifroot_response.json(), use_container_width=True)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
