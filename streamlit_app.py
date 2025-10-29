4bce10c8ffa95362be5ec5e36d073a5145998b4c
rekhamallam <48397892+rekhamallam@users.noreply.github.com>
2025-10-29 16:18:31 -0400

    Fix variable name typo for my_dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 #convert the snowpark dataframe to panda dataframw
-pd_df=mydataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
