6721d03cdf668352ca4389c508d78caa2e9d4c31
rekhamallam <48397892+rekhamallam@users.noreply.github.com>
2025-10-29 15:08:21 -0400

    Fix data display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json)
-#sf_df = st.dataframe(data=smothiefroot_response.json, use_container_width=True)
+#st.text(smoothiefroot_response.json)
+sf_df = st.dataframe(data=smothiefroot_response.json, use_container_width=True)
 
 
 # Write directly to the app
